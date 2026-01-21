e8f99a18dad60cd8e94cc9f779c28b32ae917d66
Aleksandr Orefkov <orefkov@gmail.com>
2026-01-21 18:25:30 +0300

    Add documentation

---
+++ simstr/docs_en/classsimstr_1_1_splitter_base.js
@@ -0,0 +1,5 @@
+var classsimstr_1_1_splitter_base =
+[
+    [ "is_done", "classsimstr_1_1_splitter_base.html#a4ba6176b0e6ec2230fe51d2022252801", null ],
+    [ "next", "classsimstr_1_1_splitter_base.html#a95ab08d028e2a33a90b13eae2c744c29", null ]
+];
---
+++ simstr/docs_en/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['is_5fequal_5fstr_5ftype_5fv_0',['is_equal_str_type_v',['../group___str_exprs.html#ga4cc5f9a930baa653948843b0d2284b27',1,'simstr']]]
+];
